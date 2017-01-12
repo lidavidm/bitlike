22fde9c8054bc53be1b2a3a85815e922075901df
David Li <li.davidm96@gmail.com>
2017-01-12 09:30:51 -0700

    feat: add dungeon generation/rendering

--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,29 @@ function keyToDelta(key: number, dx: number, dy: number): most.Stream<Position> 
     return keyStream(key).throttle(200).map(() => ({ x: dx, y: dy }));
 }
 
+let mapGen = new ROT.Map.Digger(80, 24);
+let map: number[][] = [];
+for (let y = 0; y < 24; y++) {
+    let row = [];
+    for (let x = 0; x < 80; x++) {
+        row.push(0);
+    }
+    map.push(row);
+}
+mapGen.create((x: number, y: number, value: number) => {
+    map[y][x] = value;
+});
+
 let player: Position = { x: 0, y: 0 };
+for (let y = 0; y < 24; y++) {
+    for (let x = 0; x < 80; x++) {
+        if (map[y][x] === 0) {
+            player.x = x;
+            player.y = y;
+            break;
+        }
+    }
+}
 
 let movementDeltas = keyToDelta(ROT.VK_UP, 0, -1)
     .merge(keyToDelta(ROT.VK_DOWN, 0, 1))
@@ -75,6 +97,15 @@ document.body.appendChild(display.getContainer());
 
 let render = function() {
     display.clear();
+
+    for (let y = 0; y < 24; y++) {
+        for (let x = 0; x < 80; x++) {
+            if (map[y][x] > 0) {
+                display.draw(x, y, " ", null, "#aaa");
+            }
+        }
+    }
+
     display.draw(player.x, player.y, "@");
     window.requestAnimationFrame(render);
 };
