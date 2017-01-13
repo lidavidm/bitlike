23934dac66acc97f8075b16d8c86684157687e0a
David Li <li.davidm96@gmail.com>
2017-01-12 17:03:11 -0700

    feat: check for collision on movement

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,18 @@ interface Position {
     y: number,
 }
 
+interface Delta {
+    dx: number,
+    dy: number,
+}
+
+type Map = number[][];
+
+interface MovementEvent {
+    map: Map,
+    delta: Delta,
+}
+
 let time = most.periodic(50, "tick");
 let keyStream = (function() {
     let stream = most.fromEvent("keydown", document.body)
@@ -50,43 +62,56 @@ let keyStream = (function() {
     };
 })();
 
-function keyToDelta(key: number, dx: number, dy: number): most.Stream<Position> {
-    return keyStream(key).throttle(200).map(() => ({ x: dx, y: dy }));
+function keyToDelta(key: number, dx: number, dy: number): most.Stream<Delta> {
+    return keyStream(key).throttle(200).map(() => ({ dx: dx, dy: dy }));
 }
 
 let mapGen = new ROT.Map.Digger(80, 24);
-let map: number[][] = [];
+let mapArray: number[][] = [];
 for (let y = 0; y < 24; y++) {
     let row = [];
     for (let x = 0; x < 80; x++) {
         row.push(0);
     }
-    map.push(row);
+    mapArray.push(row);
 }
 mapGen.create((x: number, y: number, value: number) => {
-    map[y][x] = value;
+    mapArray[y][x] = value;
 });
 
-let player: Position = { x: 0, y: 0 };
+let initialPlayerPosition: Position = { x: 0, y: 0 };
 for (let y = 0; y < 24; y++) {
     for (let x = 0; x < 80; x++) {
-        if (map[y][x] === 0) {
-            player.x = x;
-            player.y = y;
+        if (mapArray[y][x] === 0) {
+            initialPlayerPosition.x = x;
+            initialPlayerPosition.y = y;
             break;
         }
     }
 }
 
-let movementDeltas = keyToDelta(ROT.VK_UP, 0, -1)
+let map = most.just(mapArray);
+let movementDeltas: most.Stream<Delta> = keyToDelta(ROT.VK_UP, 0, -1)
     .merge(keyToDelta(ROT.VK_DOWN, 0, 1))
     .merge(keyToDelta(ROT.VK_LEFT, -1, 0))
     .merge(keyToDelta(ROT.VK_RIGHT, 1, 0));
+let movementEvents = movementDeltas.sample((map: number[][], delta: Delta) => ({
+    map: map,
+    delta: delta,
+}), map, movementDeltas);
 
-movementDeltas.observe((delta: Position) => {
-    player.x += delta.x;
-    player.y += delta.y;
-});
+let playerPosition: most.Stream<Position> = movementEvents
+    .scan((position: Position, event: MovementEvent) => {
+        console.log(position, event);
+        let newX = position.x + event.delta.dx;
+        let newY = position.y + event.delta.dy;
+
+        if (event.map[newY][newX] == 0) {
+            position.x = newX;
+            position.y = newY;
+        }
+        return position;
+    }, initialPlayerPosition);
 
 const display: any = new ROT.Display({
     width: 80,
@@ -95,7 +120,7 @@ const display: any = new ROT.Display({
 
 document.body.appendChild(display.getContainer());
 
-let render = function() {
+most.combine((playerPosition: Position, map: number[][]) => {
     display.clear();
 
     for (let y = 0; y < 24; y++) {
@@ -106,8 +131,5 @@ let render = function() {
         }
     }
 
-    display.draw(player.x, player.y, "@");
-    window.requestAnimationFrame(render);
-};
-
-render();
+    display.draw(playerPosition.x, playerPosition.y, "@");
+}, playerPosition, map).observe(() => {});
